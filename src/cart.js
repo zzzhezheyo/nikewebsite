228ea1fc7249768d1bcb3dabc0be3d5bb2bc1d1b
zzzhezheyo <810884502@qq.com>
2019-01-10 16:39:32 +0800

    fix:购物车

--- src/cart.js
+++ src/cart.js
@@ -1,216 +1,151 @@
-require(["./requirejs.config"], () => {
-	
-	require(["jquery","header","cookie"], () => {
-		$(function(){
-			//判断是否有名为shopCar的cookie
-			if(!($.cookie("shopCar"))){
-				$(".emptyCar").css({
-					"display":"block"
-				})
-				$(".haveCar").css({
-					"display":"none"
-				})
-			}else{
-				
-				$(".emptyCar").css({
-					"display":"none"
-				})
-				$(".haveCar").css({
-					"display":"block"
-				})
-				let thiscookie = $.cookie("shopCar")
-				let get = thiscookie.split(':')
-				if(get.length > 2){
-					for(let i = 0; i < get.length-2; i++){
-						$(".trtwo").after(
-							`<tr class="add">
-								<th>
-									<img src="/static/images/15409757545.png"/>
-								</th>
-								<th>
-									<span class="allxx"></span>
-								</th>
-								<th>
-									<span class="dj">
-									</span>
-								</th>
-								<th>
-									<div>
-										<a class="prev" href="javascript:;">-</a>
-										<span class="num">1</span>
-										<a class="next" href="javascript:;">+</a>
-									</div>
-								</th>
-								<th>
-									<span class="xj">
-									</span>
-								</th>
-								<th>
-									<a class="del" href="javascript:;">X</a>
-								</th>
-							</tr>`
-							
-						)
-					}
-					addfun()
-					delfn()
-				}
-				function addfun(){
-					let allxx = $(".allxx")
-					let dj = $(".dj")
-					let xj = $(".xj")
-					let num = $(".num")
-					for(let i = 0; i < allxx.length; i++){
-						let name = get[i].split('  ')[0]
-						let banben = get[i].split('  ')[2]
-						let color = get[i].split('  ')[1]
-						let mon = get[i].split('  ')[3]
-						let phnum = get[i].split('  ')[4]
-						allxx[i].innerHTML = name+"("+banben+"  "+color+")"
-						dj[i].innerHTML = mon
-						xj[i].innerHTML = mon
-						num[i].innerHTML = phnum
-					}
-					
-				}
-				addfun()
-				//计算所有价格
-				function allmoney(){
-					//获取所有数量和单价
-					let xj = $(".xj")
-					let num = $(".num")
-					let moneys = 0
-					for(let i = 0; i < num.length; i++){
-						let jg = Number(xj[i].innerHTML.split('￥')[1])
-						let sl = Number(num[i].innerHTML)
-						moneys += jg*sl
-					}
-					$("#zj").text("￥"+moneys)
-				}
-				allmoney()
-				//删除按钮
-				function delfn(){
-					let dels = $(".del")
-					for(let i = 0; i < dels.length; i++){
-						dels[i].onclick = function(){
-							this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)
-							allmoney()
-							cook()
-						}
-						
-					}
-					
-				}
-				delfn()
-				//数量增减
-				function adddel(){
-					let dels = $(".del")
-					let nums = $(".num")
-					let prevs = $(".prev")
-					let nexts = $(".next")
-					for(let i = 0; i < prevs.length; i++){
-						
-						prevs[i].onclick = function(){
-							let thisnum = Number(nums[i].innerHTML)
-							thisnum--
-							if(thisnum == 0){
-								dels[i].onclick()
-								allmoney()
-								cook()
-							}else{
-								nums[i].innerHTML = thisnum
-								allmoney()
-								cook()
-							}
-						}
-						nexts[i].onclick=function(){
-							let thisnum = Number(nums[i].innerHTML)
-							thisnum++
-							nums[i].innerHTML = thisnum
-							allmoney()
-							cook()
-						}
-					}
-				}
-				adddel()
-				//存cookie
-				function cook(){
-					let allxx = $(".allxx")
-					let dj = $(".dj")
-					let num = $(".num")
-					let cookstr = null
-					for(let i = 0 ; i < dj.length; i++){
-						let all= allxx[i].innerHTML
-						let name = all.split('(')[0]
-						let banben = all.split('(')[1].split('  ')[0]
-						let colors = all.split('(')[1].split('  ')[1].split(')')[0]
-						
-						if(i >= 1){
-							cookstr += name + "  " +colors+ "  " +banben+ "  "+dj[i].innerHTML+"  "+num[i].innerHTML+":"
-						}else{
-							cookstr = name + "  " +colors+ "  " +banben+ "  "+dj[i].innerHTML+"  "+num[i].innerHTML+":"
-						}
-					}
-					$.cookie("shopCar",cookstr, {path: "/"});
-				}
-				cook()
-				$("#gopay").on("click",function(){
-					//检测是否登陆
-					if($.cookie("username") != undefined){
-						alert("有钱吗你就买")
-					}else{
-						alert("请先登陆！")
-						location.href = "ml/login.html";
-					}
-				})
-			}
+ //业务逻辑
+ require(["./requirejs.config"], () => {
+ 	//引入index需要依赖的模块
+ 	require(["jquery", "header","footer","cookie"], () => {
+ 		 $(function(){
+		// 	$(".create").load("/html/cart.html");
+
+		 	var myCart = $.cookie('cart');
+
+		 	console.log("wo"+myCart);
+
+		 	//table
+		 	var box = $("#box");
+		 	var	table = $("table",box)[0];
+		 	var tbody = $("tbody", box)[0];
+		 
+		 	var sumAll= $("b",$("tfoot",box)[0])[0];
+		 	sumAll.innerHTML = 0;
+
+			var str = "";
+			var arr=$.cookie("cart")?($.cookie("cart")):[];
 			
-		})
-	})
-})
+			var getstr = arr.split(" ");
+	
+			var arr=[];
+			for(var i=0;i<getstr.length;i++){
+				/*var get = JSON.parse(getstr[i]);*/
+				var get = getstr[i];
+				var getobj = JSON.parse(get);
+				/*console.log(getobj);*/
+				arr.push(getobj);
+			}
+			// /*console.log(arr);*/
+			// for(var value of arr){
+			// 	for(var key in value){
+			// 		value[key]=decodeURIComponent(value[key]);
+			// 	}
+			// 	str +='<tr>'+
+			// 		  '<td><input type="checkbox" class="check" /></td>'+
+			// 		  '<td><span class="Ttitle">'+value.title+'</span></td>'+
+			// 		  '<td><span class="Tprice">'+value.price+'</span></td>'+
+			// 		  '<td><span class="Tnum">'+
+			// 		  	'<a href="javascript:;" class="prev">-</a>'+
+			// 		  	'<a href="javascript:;" class="next">+</a>'+
+			// 		  +'</span></td>'+		
+			// 		  '<td>'+
+		 //            	'<a href="javascript:;" class="delBtn">删除</a>'+
+		 //             '</td>'+
+		             
+	  //      		  '</tr>'
+			// }
+			// //加入数据
+			// // addfun();	
+			// tbody.innerHTML =str;
+			
+			// /*function addfun(){
+			// 	let Ttitle = $(".Ttitle");
+			// 	let Tprice = $(".Tprice");
+			// 	let Tnum = $(".Tnum");
+			// 	console.log(Ttitle);
 
+			// 	for(let i=0;i<Ttitle.length;i++)
+			// }*/
+			
+			for(var value of arr){
+				for(var key in value){
+					value[key]=decodeURIComponent(value[key]);
+				}
+				str +='<tr>'+
+					  '<td><input type="checkbox" class="check"/></td>'+
+					  '<td><span>'+ " " +'</span></td>'+
+					  '<td><span>'+value.title+'</span></td>'+
+					  '<td><span>'+value.price+'</span></td>'+
+					  '<td><span class="num">'+value.num+'</span></td>'+
+					  '<td>'+
+		            	// '<a href="javascript:;" class="editBtn">编辑</a>'+
+		            	// '<a href="javascript:;" class="okBtn">确定</a>'+
+		            	// '<a href="javascript:;" class="cancelBtn">取消</a>'+
+		            	'<a href="javascript:;" class="delBtn">删除</a>'+
+		             '</td>'+
+		             
+	       		  '</tr>'
+			}
+			tbody.innerHTML=str;
 
-// e=e||event;
+			var n = 0; // 记录单选按钮被选中的数量
 
-// 				var target = e.target||e.srcElement;
-// 				console.log(tr);
-// 				//找到当前的tr
-// 				var tr = target.parentNode.parentNode;
-// 				if(target.className === "delBtn"){
-// 					if(confirm("您真的不买我了吗？")){
-// 						tr.parentNode.removeChild(tr);
-// 						//如果被选中，减去
-// 					}
-// 					// reprice();
-// 				}else if(target.className==="reduce"){
-// 					if(value.num=<1){
-// 						value.num =1;
-// 					}else{
-// 						value.num--;
-// 					}
-// 					$(".renum").text(value.num);
-// 					// reprice();
-// 				}else if(target.className==="add"){
-// 					value.num++;
-// 					value.num = value.num;
+			$("table").on("click",function(e){
+				e = e || event;
+				//找到事件源
+				var target = e.target || e.srcElement;
+				//找到当前tr
+				var tr = target.parentNode.parentNode;
+				// console.log(tr);
+				if(target.className === "delBtn"){
+					if(confirm("您真的不买我了吗？")){
+						tr.parentNode.removeChild(tr);
+						//判断当前一行是否被选中
+						var check = $(".check", tr)[0];
+						//如果被选中，n减一
+						if(check.checked) n--;
+					}
+					reprice();
+				}else if(target.className === "check"){
+					target.checked? n++ : n--;
+					var aChecked = $(".check",box);
+					reprice();
+				}
 
-// 					$(".renum",tr).text(value.num);
-					
-// 					// reprice();
-// 				}
+			})
+			//
+			function reprice(){
+				var sum=0;
+				var newArr = [];
+				var tr = $("tr",tbody);
+				for(var i=0;i<tr.length;i++){
+					var check = $(".check",tr[i])[0];
+					if(check.checked===true){
+						var x1 = Number($("span",tr[i])[2].innerHTML);
+						var x2 = Number($("span",tr[i])[3].innerHTML);
+						console.log(x1);
+						console.log(x2);
+						var trMoney=x1*x2;
+						sum+=trMoney;
+						console.log(sum);
+					}
+					//修改cookie
+					var obj={
+						title:$("span",tr[i])[1].innerHTML,
+						price:$("span",tr[i])[2].innerHTML,
+						num:$("span",tr[i])[3].innerHTML
+					};
+					console.log(obj);
+					for(var key in obj){
+						obj[key]=obj[key];
+					}
+					newArr.push(obj);
+					console.log(obj);
+				}
+				sumAll.innerHTML=sum;
+				var objString = JSON.stringify(obj);
+				$.cookie("cart", objString, { path : '/'});
+				console.log($.cookie("cart"));
+			}
 
+			// console.log($.cookie('cart'));
 
-// 			})
-// 			//遍历获取价格
-// 			// function reprice(){
-// 			// 	var sum = 0 ;
-// 			// 	var newArr = [];
-// 			// 	var tr =$("tr",tbody);
-// 			// 	/*console.log(tr.length);*/
-// 			// 	for(var i=0;i<tr.length;i++){
-// 			// 		var x1 = $("span",tr[i])[1].innerHTML;
-// 			// 		var x2 = value.num;
-// 			// 		console.log(x1);
-// 			// 		console.log(x2);
-// 			// 	}
-// 			// }				
+ 		 })
+ 	})
+
+ })
--- src/js/cart.js
+++ src/js/cart.js
@@ -13,7 +13,7 @@
 		 	var box = $("#box");
 		 	var	table = $("table",box)[0];
 		 	var tbody = $("tbody", box)[0];
-		 	/*var arr = JSON.parse(tools.cookie("cart"));*/
+		 
 		 	var sumAll= $("b",$("tfoot",box)[0])[0];
 		 	sumAll.innerHTML = 0;
 
@@ -27,35 +27,123 @@
 				/*var get = JSON.parse(getstr[i]);*/
 				var get = getstr[i];
 				var getobj = JSON.parse(get);
-				console.log(getobj);
+				/*console.log(getobj);*/
 				arr.push(getobj);
 			}
-			console.log(arr);
+			// /*console.log(arr);*/
+			// for(var value of arr){
+			// 	for(var key in value){
+			// 		value[key]=decodeURIComponent(value[key]);
+			// 	}
+			// 	str +='<tr>'+
+			// 		  '<td><input type="checkbox" class="check" /></td>'+
+			// 		  '<td><span class="Ttitle">'+value.title+'</span></td>'+
+			// 		  '<td><span class="Tprice">'+value.price+'</span></td>'+
+			// 		  '<td><span class="Tnum">'+
+			// 		  	'<a href="javascript:;" class="prev">-</a>'+
+			// 		  	'<a href="javascript:;" class="next">+</a>'+
+			// 		  +'</span></td>'+		
+			// 		  '<td>'+
+		 //            	'<a href="javascript:;" class="delBtn">删除</a>'+
+		 //             '</td>'+
+		             
+	  //      		  '</tr>'
+			// }
+			// //加入数据
+			// // addfun();	
+			// tbody.innerHTML =str;
+			
+			// /*function addfun(){
+			// 	let Ttitle = $(".Ttitle");
+			// 	let Tprice = $(".Tprice");
+			// 	let Tnum = $(".Tnum");
+			// 	console.log(Ttitle);
+
+			// 	for(let i=0;i<Ttitle.length;i++)
+			// }*/
+			
 			for(var value of arr){
 				for(var key in value){
 					value[key]=decodeURIComponent(value[key]);
 				}
 				str +='<tr>'+
-					  '<td><input type="checkbox" class="check" /></td>'+
-					  '<td><span class="Ttitle">'+value.title+'</span></td>'+
-					  '<td><span class="Tprice">'+value.price+'</span></td>'+
-					  '<td><span class="Tnum">'
-					  		
-					  '</span></td>'+
+					  '<td><input type="checkbox" class="check"/></td>'+
+					  '<td><span>'+ " " +'</span></td>'+
+					  '<td><span>'+value.title+'</span></td>'+
+					  '<td><span>'+value.price+'</span></td>'+
+					  '<td><span class="num">'+value.num+'</span></td>'+
 					  '<td>'+
+		            	// '<a href="javascript:;" class="editBtn">编辑</a>'+
+		            	// '<a href="javascript:;" class="okBtn">确定</a>'+
+		            	// '<a href="javascript:;" class="cancelBtn">取消</a>'+
 		            	'<a href="javascript:;" class="delBtn">删除</a>'+
 		             '</td>'+
 		             
 	       		  '</tr>'
 			}
-			tbody.innerHTML =str;
-			
-			
+			tbody.innerHTML=str;
+
+			var n = 0; // 记录单选按钮被选中的数量
+
 			$("table").on("click",function(e){
-				
+				e = e || event;
+				//找到事件源
+				var target = e.target || e.srcElement;
+				//找到当前tr
+				var tr = target.parentNode.parentNode;
+				// console.log(tr);
+				if(target.className === "delBtn"){
+					if(confirm("您真的不买我了吗？")){
+						tr.parentNode.removeChild(tr);
+						//判断当前一行是否被选中
+						var check = $(".check", tr)[0];
+						//如果被选中，n减一
+						if(check.checked) n--;
+					}
+					reprice();
+				}else if(target.className === "check"){
+					target.checked? n++ : n--;
+					var aChecked = $(".check",box);
+					reprice();
+				}
+
 			})
+			//
+			function reprice(){
+				var sum=0;
+				var newArr = [];
+				var tr = $("tr",tbody);
+				for(var i=0;i<tr.length;i++){
+					var check = $(".check",tr[i])[0];
+					if(check.checked===true){
+						var x1 = Number($("span",tr[i])[2].innerHTML);
+						var x2 = Number($("span",tr[i])[3].innerHTML);
+						console.log(x1);
+						console.log(x2);
+						var trMoney=x1*x2;
+						sum+=trMoney;
+						console.log(sum);
+					}
+					//修改cookie
+					var obj={
+						title:$("span",tr[i])[1].innerHTML,
+						price:$("span",tr[i])[2].innerHTML,
+						num:$("span",tr[i])[3].innerHTML
+					};
+					console.log(obj);
+					for(var key in obj){
+						obj[key]=obj[key];
+					}
+					newArr.push(obj);
+					console.log(obj);
+				}
+				sumAll.innerHTML=sum;
+				var objString = JSON.stringify(obj);
+				$.cookie("cart", objString, { path : '/'});
+				console.log($.cookie("cart"));
+			}
 
-
+			// console.log($.cookie('cart'));
 
  		 })
  	})
