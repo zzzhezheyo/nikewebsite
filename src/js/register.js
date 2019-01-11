ff94a5d3479d0f283ed8a066eb27e8c6b0907ae7
zzzhezheyo <810884502@qq.com>
2019-01-11 11:09:30 +0800

    Update register.js

--- src/js/register.js
+++ src/js/register.js
@@ -18,6 +18,7 @@ require(["./requirejs.config"], () => {
 				var reg1 =/^.{6,}$/;
 				var reg2 = /^1\d{10}/;
 				console.log("text");
+				var flag = true;
 				//验证用户名	
 				if(reg.test(wname)&&!wname==""){
 					console.log("text");
@@ -25,46 +26,53 @@ require(["./requirejs.config"], () => {
 					alert("请输入正确的用户名");
 				}
 				//验证密码不能少于6位的字符
-				if(reg1.test(wpwd)){
+				if(reg1.test(wpwd)&&!wpwd==""){
 					
 				}else{
 					alert("请输入正确的密码");
-					
+					flag = false;
 				}
 				//再次验证密码
-				if(wpwd===wrePwd){
+				if(wpwd===wrePwd&&!wrePwd==""){
 					
 				} else{
 					alert("请两次输入的密码一致");
-				
+					flag = false;
 				}
 				//验证以1开头11位的手机号码
-				if(reg2.test(wtel)){
+				if(reg2.test(wtel)&&!wtel==""){
 		
 				}else{
 					alert("请输入正确的手机号码");
+					flag = false;
 				}
 				console.log("text");
 
-				$.ajax({
-					url: "http://localhost/api/v1/register.php",
-					type: "post",
-					data: {
-						name: wname,
-						tel: wtel,
-						password: wpwd
-					},
-					success: function(res){
-						console.log("t666");
-						if(res.res_code ===1){
-							alert("注册成功，马上去登录");
-							location.href = "/html/login.html";
-						}else if(res.res_code === 0){
-							alert("用户名已存在,请重新注册");
-						}
-					},
-					dataType: "json"
-				})	
+				if(flag===false){
+					alert("注册失败");
+				}else{
+					$.ajax({
+						url: "http://localhost/api/v1/register.php",
+						type: "post",
+						data: {
+							name: wname,
+							tel: wtel,
+							password: wpwd
+						},
+						success: function(res){
+							console.log("t666");
+							if(res.res_code ===1){
+								alert("注册成功，马上去登录");
+								location.href = "/html/login.html";
+							}else if(res.res_code === 0){
+								alert("用户名已存在,请重新注册");
+							}
+						},
+						dataType: "json"
+					})	
+				}
+
+				
 				return false;
 			})
 		})
